e1825f1236a2c4bc1aa99e3215387deace4ac570
spioblock <93063786+spioblock@users.noreply.github.com>
2021-10-24 13:57:15 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,11 @@
+function logo() {
+  alert('You clicked spio logo');
+}
+
+
+function seeacr() {
+  document.getElementById('t').innerHTML = "Smart Personal Identification Operations";
+}
+function hideacr() {
+  document.getElementById('t').innerHTML = "Spio";
+}
